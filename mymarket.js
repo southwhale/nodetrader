76a7fa59f837551baab0887b6a5b59d284c6b4d3
383523223@qq.com <383523223@qq.com>
2017-05-14 17:17:00 +0800

    update

--- mymarket.js
+++ mymarket.js
@@ -9,17 +9,17 @@ function Market(ctp, userID) {
 
 (function() {
 
-	this.OnRspUserLogin = function(Login, Rsp, nRequestID, bIsLast) {
-	  logger.info('OnRspUserLogin : %j, %j, %s, %s', Login, Rsp, nRequestID, bIsLast);
+	this.OnRspUserLogin = function(data, rsp, nRequestID, bIsLast) {
+	  logger.info('OnRspUserLogin : %j, %j, %s, %s', data, rsp, nRequestID, bIsLast);
 	 	console.log("SubscribeMarketData:", this.ctp.md.SubscribeMarketData(['zn1707', 'ru1709', 'rb1710']));
 	};
 
-	this.OnRspSubMarketData = function(Instrument, Rsp, nRequestID, bIsLast) {
-    console.log("OnRspSubMarketData:", Instrument, Rsp, nRequestID, bIsLast);
+	this.OnRspSubMarketData = function(data, rsp, nRequestID, bIsLast) {
+    console.log("OnRspSubMarketData:", data, rsp, nRequestID, bIsLast);
 	};
 
-	this.OnRspUnSubMarketData = function(Instrument, Rsp, nRequestID, bIsLast) {
-    console.log("OnRspSubMarketData:", Instrument, Rsp, nRequestID, bIsLast);
+	this.OnRspUnSubMarketData = function(data, rsp, nRequestID, bIsLast) {
+    console.log("OnRspSubMarketData:", data, rsp, nRequestID, bIsLast);
 	};
 
 	this.OnRtnDepthMarketData = function(data) {
