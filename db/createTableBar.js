9029f5439de2ca17ad960eb224b0f413e66822d9
383523223@qq.com <383523223@qq.com>
2018-11-10 23:41:19 +0800

    全部清空 重造

--- db/createTableBar.js
+++
@@ -1,7 +0,0 @@
-// 该模块用于创建表
-
-const Bar = require('./model/bar');
-
-Bar.drop().then(function() {
-	Bar.sync({force: true});
-});
--- db/createTableTick.js
+++
@@ -1,7 +0,0 @@
-// 该模块用于创建表
-
-const Tick = require('./model/tick');
-
-Tick.drop().then(function() {
-	Tick.sync({force: true});
-});
